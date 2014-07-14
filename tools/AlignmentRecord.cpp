da0ed0183ded26ff12ac453997a3de4f526bf7db
Andrew McPherson <amcphers@bccrc.ca>
2014-07-14 08:16:00 -0700

    tabs not spaces

--- tools/AlignmentRecord.cpp
+++ tools/AlignmentRecord.cpp
@@ -10,58 +10,58 @@
 
 int SpanningAlignmentRecord::GetOuterPosition() const
 {
-    DebugCheck(strand == "+" || strand == "-");
+	DebugCheck(strand == "+" || strand == "-");
 
-    if (strand == "+")
-    {
-        return start;
-    }
-    else
-    {
-        return end;
-    }
+	if (strand == "+")
+	{
+		return start;
+	}
+	else
+	{
+		return end;
+	}
 }
 
 std::ostream & operator<<(std::ostream &os, const SpanningAlignmentRecord& record)
 {
-    os << record.libID << "\t";
-    os << record.readID << "\t";
-    os << record.readEnd << "\t";
-    os << record.alignID << "\t";
-    os << record.chromosome << "\t";
-    os << record.strand << "\t";
-    os << record.start << "\t";
-    os << record.end << "\t";
-    os << record.score << std::endl;
-    return os;
+	os << record.libID << "\t";
+	os << record.readID << "\t";
+	os << record.readEnd << "\t";
+	os << record.alignID << "\t";
+	os << record.chromosome << "\t";
+	os << record.strand << "\t";
+	os << record.start << "\t";
+	os << record.end << "\t";
+	os << record.score << std::endl;
+	return os;
 }
 
 std::istream & operator>>(std::istream &is, SpanningAlignmentRecord& record)
 {
-    is >> record.libID;
-    is >> record.readID;
-    is >> record.readEnd;
-    is >> record.alignID;
-    is >> record.chromosome;
-    is >> record.strand;
-    is >> record.start;
-    is >> record.end;
-    is >> record.score;
-    return is;
+	is >> record.libID;
+	is >> record.readID;
+	is >> record.readEnd;
+	is >> record.alignID;
+	is >> record.chromosome;
+	is >> record.strand;
+	is >> record.start;
+	is >> record.end;
+	is >> record.score;
+	return is;
 }
 
 std::ostream & operator<<(std::ostream &os, const SplitAlignmentRecord& record)
 {
-    os << record.libID << "\t";
-    os << record.readID << "\t";
-    os << record.readEnd << "\t";
-    for (int readEnd = 0; readEnd < 2; readEnd++)
-    {
-        os << record.alignID[readEnd] << "\t";
-        os << record.chromosome[readEnd] << "\t";
-        os << record.strand[readEnd] << "\t";
-        os << record.position[readEnd] << "\t";
-    }
+	os << record.libID << "\t";
+	os << record.readID << "\t";
+	os << record.readEnd << "\t";
+	for (int readEnd = 0; readEnd < 2; readEnd++)
+	{
+		os << record.alignID[readEnd] << "\t";
+		os << record.chromosome[readEnd] << "\t";
+		os << record.strand[readEnd] << "\t";
+		os << record.position[readEnd] << "\t";
+	}
 	if (record.inserted.empty())
 	{
 		os << "-\t";
@@ -70,71 +70,71 @@ std::ostream & operator<<(std::ostream &os, const SplitAlignmentRecord& record)
 	{
 		os << record.inserted << "\t";
 	}
-    os << record.score << std::endl;
-    return os;
+	os << record.score << std::endl;
+	return os;
 }
 
 std::istream & operator>>(std::istream &is, SplitAlignmentRecord& record)
 {
-    is >> record.libID;
-    is >> record.readID;
-    is >> record.readEnd;
-    for (int readEnd = 0; readEnd < 2; readEnd++)
-    {
-        is >> record.alignID[readEnd];
-        is >> record.chromosome[readEnd];
-        is >> record.strand[readEnd];
-        is >> record.position[readEnd];
-    }
-    is >> record.inserted;
+	is >> record.libID;
+	is >> record.readID;
+	is >> record.readEnd;
+	for (int readEnd = 0; readEnd < 2; readEnd++)
+	{
+		is >> record.alignID[readEnd];
+		is >> record.chromosome[readEnd];
+		is >> record.strand[readEnd];
+		is >> record.position[readEnd];
+	}
+	is >> record.inserted;
 	if (record.inserted == "-")
 	{
 		record.inserted = "";
 	}
-    is >> record.score;
-    return is;
+	is >> record.score;
+	return is;
 }
 
 AlignmentPairKey SplitAlignmentRecord::GetAlignmentPairKey() const
 {
-    AlignmentPairKey alignPairKey;
-    alignPairKey.libID = libID;
-    alignPairKey.readID = readID;
-    for (int readEnd = 0; readEnd < 2; readEnd++)
-    {
-        alignPairKey.alignID[readEnd] = alignID[readEnd];
-    }
-    return alignPairKey;
+	AlignmentPairKey alignPairKey;
+	alignPairKey.libID = libID;
+	alignPairKey.readID = readID;
+	for (int readEnd = 0; readEnd < 2; readEnd++)
+	{
+		alignPairKey.alignID[readEnd] = alignID[readEnd];
+	}
+	return alignPairKey;
 }
 
 std::ostream & operator<<(std::ostream &os, const ClusterMemberRecord& record)
 {
-    os << record.clusterID << "\t";
-    os << record.clusterEnd << "\t";
-    os << record.libID << "\t";
-    os << record.readID << "\t";
-    os << record.readEnd << "\t";
-    os << record.alignID << std::endl;
-    return os;
+	os << record.clusterID << "\t";
+	os << record.clusterEnd << "\t";
+	os << record.libID << "\t";
+	os << record.readID << "\t";
+	os << record.readEnd << "\t";
+	os << record.alignID << std::endl;
+	return os;
 }
 
 std::istream & operator>>(std::istream &is, ClusterMemberRecord& record)
 {
-    is >> record.clusterID;
-    is >> record.clusterEnd;
-    is >> record.libID;
-    is >> record.readID;
-    is >> record.readEnd;
-    is >> record.alignID;
-    return is;
+	is >> record.clusterID;
+	is >> record.clusterEnd;
+	is >> record.libID;
+	is >> record.readID;
+	is >> record.readEnd;
+	is >> record.alignID;
+	return is;
 }
 
 ReadRecord ClusterMemberRecord::GetReadRecord() const
 {
-    ReadRecord record;
-    record.libID = libID;
-    record.readID = readID;
-    return record;
+	ReadRecord record;
+	record.libID = libID;
+	record.readID = readID;
+	return record;
 }
 
 
--- tools/AlignmentRecord.h
+++ tools/AlignmentRecord.h
@@ -13,30 +13,30 @@
 
 struct ReadRecord
 {
-    int libID;
-    int readID;
+	int libID;
+	int readID;
 };
 
 struct AlignmentPairKey
 {
-    int libID;
-    int readID;
-    int alignID[2];
+	int libID;
+	int readID;
+	int alignID[2];
 };
 
 struct SpanningAlignmentRecord
 {
-    int libID;
-    int readID;
-    int readEnd;
-    int alignID;
-    string chromosome;
-    string strand;
-    int start;
-    int end;
-    int score;
+	int libID;
+	int readID;
+	int readEnd;
+	int alignID;
+	string chromosome;
+	string strand;
+	int start;
+	int end;
+	int score;
 
-    int GetOuterPosition() const;
+	int GetOuterPosition() const;
 };
 
 std::ostream & operator<<(std::ostream &os, const SpanningAlignmentRecord& record);
@@ -45,17 +45,17 @@ std::istream & operator>>(std::istream &is, SpanningAlignmentRecord& record);
 
 struct SplitAlignmentRecord
 {
-    int libID;
-    int readID;
-    int readEnd;
-    int alignID[2];
-    string chromosome[2];
-    string strand[2];
-    int position[2];
-    string inserted;
-    int score;
+	int libID;
+	int readID;
+	int readEnd;
+	int alignID[2];
+	string chromosome[2];
+	string strand[2];
+	int position[2];
+	string inserted;
+	int score;
 
-    AlignmentPairKey GetAlignmentPairKey() const;
+	AlignmentPairKey GetAlignmentPairKey() const;
 };
 
 std::ostream & operator<<(std::ostream &os, const SplitAlignmentRecord& record);
@@ -64,14 +64,14 @@ std::istream & operator>>(std::istream &is, SplitAlignmentRecord& record);
 
 struct ClusterMemberRecord
 {
-    int clusterID;
-    int clusterEnd;
-    int libID;
-    int readID;
-    int readEnd;
-    int alignID;
+	int clusterID;
+	int clusterEnd;
+	int libID;
+	int readID;
+	int readEnd;
+	int alignID;
 
-    ReadRecord GetReadRecord() const;
+	ReadRecord GetReadRecord() const;
 };
 
 std::ostream & operator<<(std::ostream &os, const ClusterMemberRecord& record);
@@ -81,106 +81,106 @@ std::istream & operator>>(std::istream &is, ClusterMemberRecord& record);
 template<typename TRecordType>
 std::ostream & operator<<(std::ostream &os, const vector<TRecordType>& records)
 {
-    for (typename vector<TRecordType>::const_iterator recordIter = records.begin(); recordIter != records.end(); recordIter++)
-    {
-        os << *recordIter;
-    }
+	for (typename vector<TRecordType>::const_iterator recordIter = records.begin(); recordIter != records.end(); recordIter++)
+	{
+		os << *recordIter;
+	}
 
-    return os;
+	return os;
 }
 
 template<typename TRecordType>
 bool ReadEqual(const TRecordType& a, const TRecordType& b)
 {
-    return (a.libID == b.libID &&
-            a.readID == b.readID);
+	return (a.libID == b.libID &&
+			a.readID == b.readID);
 }
 
 template<typename TRecordType>
 bool ClusterReadEqual(const TRecordType& a, const TRecordType& b)
 {
-    return (a.clusterID == b.clusterID &&
-            a.libID == b.libID &&
-            a.readID == b.readID);
+	return (a.clusterID == b.clusterID &&
+			a.libID == b.libID &&
+			a.readID == b.readID);
 }
 
 template<typename TRecordType>
 bool ClusterEqual(const TRecordType& a, const TRecordType& b)
 {
-    return (a.clusterID == b.clusterID);
+	return (a.clusterID == b.clusterID);
 }
 
 template<typename TRecordType>
 class GroupedRecordsStream
 {
 public:
-    GroupedRecordsStream(std::istream& is) : mStream(is)
-    {
-        mGood = (bool)(is >> mNextRecord);
-    }
-    
-    template<typename TEqualFunc>
-    bool Next(vector<TRecordType>& records, TEqualFunc eq)
-    {
-        if (!mGood)
-        {
-            return false;
-        }
-        
-        records.clear();
-        records.push_back(mNextRecord);
-        
-        while ((mGood = (bool)(mStream >> mNextRecord)))
-        {
-            if (!eq(records.front(), mNextRecord))
-            {
-                break;
-            }
-            else
-            {
-                records.push_back(mNextRecord);
-            }
-        }
-        
-        return true;
-    }
-    
+	GroupedRecordsStream(std::istream& is) : mStream(is)
+	{
+		mGood = (bool)(is >> mNextRecord);
+	}
+	
+	template<typename TEqualFunc>
+	bool Next(vector<TRecordType>& records, TEqualFunc eq)
+	{
+		if (!mGood)
+		{
+			return false;
+		}
+		
+		records.clear();
+		records.push_back(mNextRecord);
+		
+		while ((mGood = (bool)(mStream >> mNextRecord)))
+		{
+			if (!eq(records.front(), mNextRecord))
+			{
+				break;
+			}
+			else
+			{
+				records.push_back(mNextRecord);
+			}
+		}
+		
+		return true;
+	}
+	
 protected:
-    std::istream& mStream;
-    TRecordType mNextRecord;
-    bool mGood;
+	std::istream& mStream;
+	TRecordType mNextRecord;
+	bool mGood;
 };
 
 inline bool operator==(const ReadRecord& record1, const ReadRecord& record2)
 {
-    return record1.libID == record2.libID &&
-           record1.readID == record2.readID;
+	return record1.libID == record2.libID &&
+		   record1.readID == record2.readID;
 }
 
 inline size_t hash_value(const ReadRecord& record)
 {
-    size_t seed = 0;
-    hash_combine(seed, record.libID);
-    hash_combine(seed, record.readID);
-    return seed;
+	size_t seed = 0;
+	hash_combine(seed, record.libID);
+	hash_combine(seed, record.readID);
+	return seed;
 }
 
 inline bool operator==(const AlignmentPairKey& alignPairKey1, const AlignmentPairKey& alignPairKey2)
 {
-    return alignPairKey1.libID == alignPairKey2.libID &&
-           alignPairKey1.readID == alignPairKey2.readID &&
-           alignPairKey1.alignID[0] == alignPairKey2.alignID[0] &&
-           alignPairKey1.alignID[1] == alignPairKey2.alignID[1];
+	return alignPairKey1.libID == alignPairKey2.libID &&
+		   alignPairKey1.readID == alignPairKey2.readID &&
+		   alignPairKey1.alignID[0] == alignPairKey2.alignID[0] &&
+		   alignPairKey1.alignID[1] == alignPairKey2.alignID[1];
 }
 
 inline size_t hash_value(const AlignmentPairKey& alignPairKey)
 {
-    size_t seed = 0;
-    hash_combine(seed, alignPairKey.libID);
-    hash_combine(seed, alignPairKey.readID);
-    hash_combine(seed, alignPairKey.alignID[0]);
-    hash_combine(seed, alignPairKey.alignID[1]);
-    return seed;
+	size_t seed = 0;
+	hash_combine(seed, alignPairKey.libID);
+	hash_combine(seed, alignPairKey.readID);
+	hash_combine(seed, alignPairKey.alignID[0]);
+	hash_combine(seed, alignPairKey.alignID[1]);
+	return seed;
 }
 
 
