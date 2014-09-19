3e99c08d0672ca7be7022cda5401b77e8818afb1
Andrew McPherson <andrew.mcpherson@gmail.com>
2014-09-19 12:28:50 -0700

    destruct wrapper now working

--- wrappers/destruct.py
+++ wrappers/destruct.py
@@ -14,13 +14,38 @@ class DestructWrapper(object):
 
     def __init__(self, install_directory):
 
-        self.config_filename = os.path.join(install_directory, 'config.ini')
+        self.install_directory = install_directory
+
+        self.ref_data_directory = os.path.join(self.install_directory, 'data')
+        self.user_config_filename = os.path.join(self.install_directory, 'user_config.py')
+
+        self.createref_script = os.path.join(os.path.dirname(__file__), os.path.pardir, 'createref.py')
         self.destruct_script = os.path.join(os.path.dirname(__file__), os.path.pardir, 'destruct.py')
 
 
-    def install(self):
+    def install(self, **kwargs):
 
-        pass
+        Sentinal = utils.SentinalFactory(os.path.join(self.install_directory, 'sentinal_'), kwargs)
+
+        with Sentinal('createref') as sentinal:
+
+            if sentinal.unfinished:
+
+                utils.makedirs(self.install_directory)
+                
+                with open(self.user_config_filename, 'w') as user_config_file:
+                    if kwargs.get('chromosomes', None) is not None:
+                        chromosomes = kwargs['chromosomes']
+                        ensembl_assemblies = ['chromosome.'+a for a in chromosomes]
+                        user_config_file.write('chromosomes = '+repr(chromosomes)+'\n')
+                        user_config_file.write('ensembl_assemblies = '+repr(ensembl_assemblies)+'\n')
+
+                createref_cmd = [sys.executable]
+                createref_cmd += [self.createref_script]
+                createref_cmd += [self.ref_data_directory]
+                createref_cmd += ['-c', self.user_config_filename]
+
+                subprocess.check_call(createref_cmd)
 
 
     def run(self, temp_directory, bam_filenames, output_filename):
@@ -41,13 +66,14 @@ class DestructWrapper(object):
         destruct_cmd = list()
         destruct_cmd += [sys.executable]
         destruct_cmd += [self.destruct_script]
+        destruct_cmd += [self.ref_data_directory]
         destruct_cmd += [bam_list_filename]
         destruct_cmd += [breakpoints_filename]
         destruct_cmd += [breakreads_filename]
         destruct_cmd += [plots_tar_filename]
-        destruct_cmd += ['--config', self.config_filename]
+        destruct_cmd += ['--config', self.user_config_filename]
         destruct_cmd += ['--tmp', destruct_tmp_directory]
-        destruct_cmd += ['--nocleanup', '--repopulate', '--maxjobs', '4', '--verbose']
+        destruct_cmd += ['--nocleanup', '--repopulate', '--maxjobs', '4', '--loglevel', 'DEBUG']
 
         subprocess.check_call(destruct_cmd)
 
@@ -56,11 +82,12 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('install_directory', help='Destruct installation directory')
+    parser.add_argument('--chromosomes', nargs='*', type=str, default=None, help='Reference chromosomes')
     args = parser.parse_args()
 
-    delly = DellyWrapper(args.install_directory)
+    delly = DestructWrapper(args.install_directory)
 
-    delly.install()
+    delly.install(chromosomes=args.chromosomes)
 
 
 
