069e2389f6bf1137df1fecce2448a52f61dbc347
Andrew McPherson <andrew.mcpherson@gmail.com>
2015-01-02 09:04:01 -0800

    lumpy wrapper now working

--- wrappers/lumpysv.py
+++ wrappers/lumpysv.py
@@ -16,7 +16,7 @@ import pypeliner.commandline
 
 class LumpySVWrapper(object):
 
-    features = ['tumour_count', 'num_split']
+    features = ['evidence_set_score', 'read_count']
 
     def __init__(self, install_directory):
 
@@ -188,11 +188,18 @@ class LumpySVWrapper(object):
 
         utils.makedirs(temp_directory)
 
+        sample_id_map = {
+            1:'tumour_pe',
+            2:'tumour_sr',
+            3:'normal_pe',
+            4:'normal_sr',
+        }
+
         tumour_args = self.prepare(tumour_bam, os.path.join(temp_directory, 'tumour.'), 1, 2)
 
         normal_args = self.prepare(normal_bam, os.path.join(temp_directory, 'normal.'), 3, 4)
 
-        results_bed = os.path.join(temp_directory, 'results.bedpe')
+        results_bedpe = os.path.join(temp_directory, 'results.bedpe')
 
         pypeliner.commandline.execute(
             *'{lumpy_bin} \
@@ -210,15 +217,72 @@ class LumpySVWrapper(object):
                 .split()
             )
 
+        bedpe_columns = [
+            'chromosome_1',
+            'interval_1_start',
+            'interval_1_end',
+            'chromosome_2',
+            'interval_2_start',
+            'interval_2_end',
+            'prediction_id',
+            'evidence_set_score',
+            'strand_1',
+            'strand_2',
+            'type',
+            'id_of_samples',
+            'strand_configurations',
+            'breakpoint_max_prob',
+            'segment_95_prob',
+        ]
 
-    def convert_output(self, vcf_filename, bam_filenames, table_filename):
+        results = pd.read_csv(results_bedpe, sep='\t', names=bedpe_columns,
+            converters={'chromosome_1':str, 'chromosome_2':str})
 
-        pass
+        column_prefixes = [
+            ('type', 'TYPE:'),
+            ('id_of_samples', 'IDS:'),
+            ('strand_configurations', 'STRANDS:'),
+            ('breakpoint_max_prob', 'MAX:'),
+            ('segment_95_prob', '95:'),
+        ]
 
+        for column, prefix in column_prefixes:
+            results[column] = results[column].str.replace(prefix, '')
 
-    def merge_tables(self, input_filenames, output_filename):
+        results['position_1'] = None
+        results['position_2'] = None
 
-        pass
+        results[['position_1', 'position_2']] = results['breakpoint_max_prob'].str.split(';').apply(pd.Series, 1)
+
+        results['position_1'] = results['position_1'].str.split(':').apply(lambda a: a[1]).astype(int)
+        results['position_2'] = results['position_2'].str.split(':').apply(lambda a: a[1]).astype(int)
+
+        read_counts = list()
+
+        for prediction_id, id_of_samples in results[['prediction_id', 'id_of_samples']].values:
+            for sample_id, read_count in (a.split(',') for a in id_of_samples.split(';')):
+                read_counts.append((prediction_id, int(sample_id), int(read_count)))
+
+        read_counts = pd.DataFrame(read_counts, columns=('prediction_id', 'sample_id', 'read_count'))
+
+        read_counts = read_counts.set_index(['prediction_id', 'sample_id'])['read_count']\
+                                 .unstack()\
+                                 .fillna(0)\
+                                 .rename(columns=sample_id_map)
+
+        # Filter germline
+        read_counts = read_counts[(read_counts['normal_pe'] == 0) & (read_counts['normal_sr'] == 0)]
+
+        read_counts = read_counts.rename(columns={'tumour_pe':'pe_read_count',
+                                                  'tumour_sr':'sr_read_count'})
+
+        read_counts['read_count'] = read_counts['pe_read_count'] + read_counts['sr_read_count']
+
+        read_counts = read_counts.drop(['normal_pe', 'normal_sr'], axis=1)
+
+        results = results.merge(read_counts, left_on='prediction_id', right_index=True)
+
+        results.to_csv(output_filename, sep='\t', index=False)
 
 
 if __name__ == '__main__':
