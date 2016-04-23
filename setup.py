99014d7e2ee8da5595cc42151969f7512244d331
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-04-23 12:37:40 -0700

    build: versioneer for versions

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
+import versioneer
 
 setup(
     name='destruct',
-    version='0.3.1',
     packages=find_packages(),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Destruct is a tool for joint prediction of rearrangement breakpoints from single or multiple tumour samples',
+    author='Andrew McPherson',
+    author_email='andrew.mcpherson@gmail.com',
+    url='http://bitbucket.org/dranew/destruct',
+    download_url='https://bitbucket.org/dranew/destruct/get/v{}.tar.gz'.format(versioneer.get_version()),
+    keywords=['scientific', 'sequence analysis', 'cancer'],
+    classifiers=[],
     scripts=[
         'destruct/run_destruct.py',
         'destruct/create_ref_data.py',
     ],
     package_data={'destruct': ['data/*_chr_map.tsv']},
 )
+
