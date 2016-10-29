ed79383fd50f24223395fcff6ff4d9f3aa7b729b
dranew <andrew.mcpherson@gmail.com>
2016-10-29 14:45:48 -0700

    benchmark: destruct config in tool defs kwargs

--- destruct/benchmark/wrappers/destruct/workflow.py
+++ destruct/benchmark/wrappers/destruct/workflow.py
@@ -47,7 +47,7 @@ def create_destruct_wrapper_workflow(bam_filenames, output_filename, raw_data_di
 
 
 def setup_destruct(test_config, **kwargs):
-    config = {}
+    config = kwargs.get('config', {})
     
     if test_config.get('chromosomes', None) is not None:
         config = {
