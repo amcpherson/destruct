d7d6bc707ad5a4e465d8ea125ad77b3a18770aa2
Andrew McPherson <andrew.mcpherson@gmail.com>
2018-10-03 07:08:00 -0700

    refactor: separate workflows to segregate nested parallelization efficiently

--- destruct/workflow.py
+++ destruct/workflow.py
@@ -19,6 +19,272 @@ medmem = {'mem': 8, 'num_retry': 2, 'mem_retry_factor': 2}
 himem = {'mem': 16, 'num_retry': 2, 'mem_retry_factor': 2}
 
 
+def align(
+    stats_filename,
+    reads1_filename,
+    reads2_filename,
+    sample1_filename,
+    sample2_filename,
+    spanning_filename,
+    split_filename,
+    score_stats_filename,
+    library_id,
+    config,
+):
+    workflow = pypeliner.workflow.Workflow()
+
+    # Retrieve discordant reads and stats from bam files
+    workflow.transform(
+        name='readstats',
+        ctx=lowmem,
+        func=destruct.tasks.read_stats,
+        ret=mgd.TempOutputObj('stats'),
+        args=(
+            mgd.InputFile(stats_filename),
+            config['fragment_length_num_stddevs'],
+        ),
+    )
+
+    # Align a sample of reads and calculate alignment statistics
+    workflow.transform(
+        name='prepseed_sample',
+        ctx=medmem,
+        func=destruct.tasks.prepare_seed_fastq,
+        args=(
+            mgd.InputFile(sample1_filename),
+            mgd.InputFile(sample2_filename),
+            36,
+            mgd.TempOutputFile('sample.seed'),
+        ),
+    )
+
+    workflow.commandline(
+        name='bwtrealign_sample',
+        ctx=medmem,
+        args=(
+            'bowtie',
+            config['genome_fasta'],
+            mgd.TempInputFile('sample.seed'),
+            '--chunkmbs', '512',
+            '-k', '1000',
+            '-m', '1000',
+            '--strata',
+            '--best',
+            '-S',
+            '|',
+            'destruct_aligntrue',
+            '-a', '-',
+            '-1', mgd.InputFile(sample1_filename),
+            '-2', mgd.InputFile(sample2_filename),
+            '-r', config['genome_fasta'],
+            '-g', config['gap_score'],
+            '-x', config['mismatch_score'],
+            '-m', config['match_score'],
+            '--flmin', mgd.TempInputObj('stats').prop('fragment_length_min'),
+            '--flmax', mgd.TempInputObj('stats').prop('fragment_length_max'),
+            '-s', mgd.TempOutputFile('samples.align.true'),
+        ),
+    )
+
+    workflow.transform(
+        name='scorestats',
+        ctx=medmem,
+        func=destruct.score_stats.create_score_stats,
+        args=(
+            mgd.TempInputFile('samples.align.true'),
+            config['match_score'],
+            mgd.OutputFile(score_stats_filename),
+        ),
+    )
+
+    # Split discordant fastqs and align
+    workflow.transform(
+        name='splitfastq1',
+        ctx=lowmem,
+        func=destruct.tasks.split_fastq,
+        args=(
+            mgd.InputFile(reads1_filename),
+            int(config['reads_per_split']),
+            mgd.TempOutputFile('reads1', 'byread'),
+        ),
+    )
+
+    workflow.transform(
+        name='splitfastq2',
+        ctx=lowmem,
+        func=destruct.tasks.split_fastq,
+        args=(
+            mgd.InputFile(reads2_filename),
+            int(config['reads_per_split']),
+            mgd.TempOutputFile('reads2', 'byread', axes_origin=[]),
+        ),
+    )
+
+    workflow.transform(
+        name='prepseed',
+        axes=('byread',),
+        ctx=medmem,
+        func=destruct.tasks.prepare_seed_fastq,
+        args=(
+            mgd.TempInputFile('reads1', 'byread'),
+            mgd.TempInputFile('reads2', 'byread'),
+            36,
+            mgd.TempOutputFile('reads.seed', 'byread'),
+        ),
+    )
+
+    workflow.commandline(
+        name='bwtrealign',
+        axes=('byread',),
+        ctx=medmem,
+        args=(
+            'bowtie',
+            config['genome_fasta'],
+            mgd.TempInputFile('reads.seed', 'byread'),
+            '--chunkmbs', '512',
+            '-k', '1000',
+            '-m', '1000',
+            '--strata',
+            '--best',
+            '-S',
+            '|',
+            'destruct_realign2',
+            '-l', library_id,
+            '-a', '-',
+            '-1', mgd.TempInputFile('reads1', 'byread'),
+            '-2', mgd.TempInputFile('reads2', 'byread'),
+            '-r', config['genome_fasta'],
+            '-g', config['gap_score'],
+            '-x', config['mismatch_score'],
+            '-m', config['match_score'],
+            '--flmin', mgd.TempInputObj('stats').prop('fragment_length_min'),
+            '--flmax', mgd.TempInputObj('stats').prop('fragment_length_max'),
+            '--tchimer', config['chimeric_threshold'],
+            '--talign', config['alignment_threshold'],
+            '--pchimer', config['chimeric_prior'],
+            '--tvalid', config['readvalid_threshold'],
+            '-z', mgd.InputFile(score_stats_filename),
+            '--span', mgd.TempOutputFile('spanning.alignments', 'byread'),
+            '--split', mgd.TempOutputFile('split.alignments', 'byread'),
+        ),
+    )
+
+    workflow.transform(
+        name='merge_spanning_1',
+        ctx=lowmem,
+        func=destruct.tasks.merge_files_by_line,
+        args=(
+            mgd.TempInputFile('spanning.alignments', 'byread'),
+            mgd.OutputFile(spanning_filename),
+        ),
+    )
+
+    workflow.transform(
+        name='merge_split_1',
+        axes=('bylibrary',),
+        ctx=lowmem,
+        func=destruct.tasks.merge_files_by_line,
+        args=(
+            mgd.TempInputFile('split.alignments', 'byread'),
+            mgd.OutputFile(split_filename),
+        ),
+    )
+
+    return workflow
+
+
+def realign(
+    reads1_filename,
+    reads2_filename,
+    clusters_filename,
+    breakpoints_filename,
+    likelihoods_filename,
+    spanning_alignments_filename,
+    score_stats_filename,
+    stats,
+    library_id,
+    config,
+):
+    workflow = pypeliner.workflow.Workflow()
+
+    # Split discordant fastqs and align
+    workflow.transform(
+        name='splitfastq1',
+        ctx=lowmem,
+        func=destruct.tasks.split_fastq,
+        args=(
+            mgd.InputFile(reads1_filename),
+            int(config['reads_per_split']),
+            mgd.TempOutputFile('reads1', 'byread'),
+        ),
+    )
+
+    workflow.transform(
+        name='splitfastq2',
+        ctx=lowmem,
+        func=destruct.tasks.split_fastq,
+        args=(
+            mgd.InputFile(reads2_filename),
+            int(config['reads_per_split']),
+            mgd.TempOutputFile('reads2', 'byread', axes_origin=[]),
+        ),
+    )
+
+    # Realign reads to breakpoints
+    workflow.commandline(
+        name='realigntobreaks',
+        axes=('byread',),
+        ctx=medmem,
+        args=(
+            'destruct_realigntobreaks2',
+            '-r', config['genome_fasta'],
+            '-b', mgd.InputFile(breakpoints_filename),
+            '-c', mgd.InputFile(clusters_filename),
+            '-g', config['gap_score'],
+            '-x', config['mismatch_score'],
+            '-m', config['match_score'],
+            '--flmax', stats.fragment_length_max,
+            '--span', mgd.InputFile(spanning_alignments_filename),
+            '-1', mgd.TempInputFile('reads1', 'byread'),
+            '-2', mgd.TempInputFile('reads2', 'byread'),
+            '--realignments', mgd.TempOutputFile('realignments', 'byread', axes_origin=[]),
+            '-l', library_id,
+        ),
+    )
+
+    # Calculate likelihoods based on realignments
+    workflow.transform(
+        name='calculate_realignment_likelihoods',
+        axes=('byread',),
+        ctx=medmem,
+        func=destruct.predict_breaks.calculate_realignment_likelihoods,
+        args=(
+            mgd.InputFile(breakpoints_filename),
+            mgd.TempInputFile('realignments', 'byread'),
+            mgd.InputFile(score_stats_filename),
+            mgd.TempOutputFile('likelihoods_2', 'byread'),
+            config['match_score'],
+            stats.fragment_length_mean,
+            stats.fragment_length_stddev,
+        ),
+    )
+
+    workflow.transform(
+        name='merge_likelihoods_1',
+        axes=('',),
+        ctx=lowmem,
+        func=destruct.tasks.merge_sorted_files_by_line,
+        args=(
+            mgd.TempInputFile('likelihoods_2', 'byread'),
+            mgd.OutputFile(likelihoods_filename),
+            mgd.TempSpace('merge_likelihoods_1_temp'),
+            '1',
+        ),
+    )
+
+    return workflow
+
+
 def create_destruct_workflow(
     bam_filenames,
     breakpoint_table,
@@ -48,13 +314,10 @@ def create_destruct_workflow(
     workflow = pypeliner.workflow.Workflow()
 
     # Set the library ids
-    
     workflow.setobj(
         obj=mgd.TempOutputObj('library_id', 'bylibrary'),
         value=destruct.tasks.create_library_ids(bam_filenames.keys()),
     )
-
-    # Retrieve discordant reads and stats from bam files
 
     workflow.commandline(
         name='bamdisc',
@@ -76,6 +339,7 @@ def create_destruct_workflow(
         ),
     )
 
+    # Retrieve discordant reads and stats from bam files
     workflow.transform(
         name='readstats',
         axes=('bylibrary',),
@@ -88,146 +352,22 @@ def create_destruct_workflow(
         ),
     )
 
-    # Align a sample of reads and calculate alignment statistics
-
-    workflow.transform(
-        name='prepseed_sample',
+    workflow.subworkflow(
+        name='align',
         axes=('bylibrary',),
-        ctx=medmem,
-        func=destruct.tasks.prepare_seed_fastq,
+        func=align,
         args=(
+            mgd_stats.as_input(),
+            mgd_reads_1.as_input(),
+            mgd_reads_2.as_input(),
             mgd_sample_1.as_input(),
             mgd_sample_2.as_input(),
-            36,
-            mgd.TempOutputFile('sample.seed', 'bylibrary'),
-        ),
-    )
-
-    workflow.commandline(
-        name='bwtrealign_sample',
-        axes=('bylibrary',),
-        ctx=medmem,
-        args=(
-            'bowtie',
-            config['genome_fasta'],
-            mgd.TempInputFile('sample.seed', 'bylibrary'),
-            '--chunkmbs', '512',
-            '-k', '1000',
-            '-m', '1000',
-            '--strata',
-            '--best',
-            '-S',
-            '|',
-            'destruct_aligntrue',
-            '-a', '-',
-            '-1', mgd_sample_1.as_input(),
-            '-2', mgd_sample_2.as_input(),
-            '-r', config['genome_fasta'],
-            '-g', config['gap_score'],
-            '-x', config['mismatch_score'],
-            '-m', config['match_score'],
-            '--flmin', mgd.TempInputObj('stats', 'bylibrary').prop('fragment_length_min'),
-            '--flmax', mgd.TempInputObj('stats', 'bylibrary').prop('fragment_length_max'),
-            '-s', mgd.TempOutputFile('samples.align.true', 'bylibrary'),
-        ),
-    )
-
-    workflow.transform(
-        name='scorestats',
-        axes=('bylibrary',),
-        ctx=medmem,
-        func=destruct.score_stats.create_score_stats,
-        args=(
-            mgd.TempInputFile('samples.align.true', 'bylibrary'),
-            config['match_score'],
-            mgd.TempOutputFile('score.stats', 'bylibrary'),
-        ),
-    )
-
-    # Split discordant fastqs and align
-
-    workflow.transform(
-        name='splitfastq1',
-        axes=('bylibrary',),
-        ctx=lowmem,
-        func=destruct.tasks.split_fastq,
-        args=(
-            mgd_reads_1.as_input(),
-            int(config['reads_per_split']),
-            mgd.TempOutputFile('reads1', 'bylibrary', 'byread'),
-        ),
-    )
-
-    workflow.transform(
-        name='splitfastq2',
-        axes=('bylibrary',),
-        ctx=lowmem,
-        func=destruct.tasks.split_fastq,
-        args=(
-            mgd_reads_2.as_input(),
-            int(config['reads_per_split']),
-            mgd.TempOutputFile('reads2', 'bylibrary', 'byread', axes_origin=[]),
-        ),
-    )
-
-    workflow.transform(
-        name='prepseed',
-        axes=('bylibrary', 'byread'),
-        ctx=medmem,
-        func=destruct.tasks.prepare_seed_fastq,
-        args=(
-            mgd.TempInputFile('reads1', 'bylibrary', 'byread'),
-            mgd.TempInputFile('reads2', 'bylibrary', 'byread'),
-            36,
-            mgd.TempOutputFile('reads.seed', 'bylibrary', 'byread'),
-        ),
-    )
-
-    workflow.commandline(
-        name='bwtrealign',
-        axes=('bylibrary', 'byread'),
-        ctx=medmem,
-        args=(
-            'bowtie',
-            config['genome_fasta'],
-            mgd.TempInputFile('reads.seed', 'bylibrary', 'byread'),
-            '--chunkmbs', '512',
-            '-k', '1000',
-            '-m', '1000',
-            '--strata',
-            '--best',
-            '-S',
-            '|',
-            'destruct_realign2',
-            '-l', mgd.TempInputObj('library_id', 'bylibrary'),
-            '-a', '-',
-            '-1', mgd.TempInputFile('reads1', 'bylibrary', 'byread'),
-            '-2', mgd.TempInputFile('reads2', 'bylibrary', 'byread'),
-            '-r', config['genome_fasta'],
-            '-g', config['gap_score'],
-            '-x', config['mismatch_score'],
-            '-m', config['match_score'],
-            '--flmin', mgd.TempInputObj('stats', 'bylibrary').prop('fragment_length_min'),
-            '--flmax', mgd.TempInputObj('stats', 'bylibrary').prop('fragment_length_max'),
-            '--tchimer', config['chimeric_threshold'],
-            '--talign', config['alignment_threshold'],
-            '--pchimer', config['chimeric_prior'],
-            '--tvalid', config['readvalid_threshold'],
-            '-z', mgd.TempInputFile('score.stats', 'bylibrary'),
-            '--span', mgd.TempOutputFile('spanning.alignments', 'bylibrary', 'byread'),
-            '--split', mgd.TempOutputFile('split.alignments', 'bylibrary', 'byread'),
-        ),
-    )
-
-    workflow.transform(
-        name='merge_spanning_1',
-        axes=('bylibrary',),
-        ctx=lowmem,
-        func=destruct.tasks.merge_files_by_line,
-        args=(
-            mgd.TempInputFile('spanning.alignments', 'bylibrary', 'byread'),
             mgd.TempOutputFile('spanning.alignments_1', 'bylibrary'),
-        ),
+            mgd.TempOutputFile('split.alignments', 'bylibrary'),
+            mgd.TempOutputFile('score.stats', 'bylibrary'),
+            mgd.TempInputObj('library_id', 'bylibrary'),
+            config,
+        )
     )
 
     workflow.commandline(
@@ -240,17 +380,6 @@ def create_destruct_workflow(
             '-a', mgd.TempInputFile('spanning.alignments_1', 'bylibrary'),
             '-r', config['satellite_regions'],
             '>', mgd.TempOutputFile('spanning.alignments', 'bylibrary'),
-        ),
-    )
-
-    workflow.transform(
-        name='merge_split_1',
-        axes=('bylibrary',),
-        ctx=lowmem,
-        func=destruct.tasks.merge_files_by_line,
-        args=(
-            mgd.TempInputFile('split.alignments', 'bylibrary', 'byread'),
-            mgd.TempOutputFile('split.alignments', 'bylibrary'),
         ),
     )
 
@@ -277,7 +406,6 @@ def create_destruct_workflow(
     )
 
     # Cluster spanning reads
-
     workflow.setobj(
         obj=mgd.TempOutputObj('chrom.args', 'bychromarg'),
         value=destruct.tasks.generate_chromosome_args(config['chromosomes']),
@@ -308,9 +436,8 @@ def create_destruct_workflow(
             '--fragmax', config['fragment_length_max'],
         ),
     )
-    
-    # Predict breakpoints from split reads
 
+    # Predict breakpoints from split reads
     workflow.transform(
         name='predict_breaks',
         axes=('bychromarg',),
@@ -337,56 +464,21 @@ def create_destruct_workflow(
         ),
     )
 
-    # Realign reads to breakpoints
-
-    workflow.commandline(
-        name='realigntobreaks',
-        axes=('bylibrary', 'byread'),
-        ctx=medmem,
-        args=(
-            'destruct_realigntobreaks2',
-            '-r', config['genome_fasta'],
-            '-b', mgd.TempInputFile('breakpoints_2'),
-            '-c', mgd.TempInputFile('clusters'),
-            '-g', config['gap_score'],
-            '-x', config['mismatch_score'],
-            '-m', config['match_score'],
-            '--flmax', mgd.TempInputObj('stats', 'bylibrary').prop('fragment_length_max'),
-            '--span', mgd.TempInputFile('spanning.alignments', 'bylibrary', 'byread'),
-            '-1', mgd.TempInputFile('reads1', 'bylibrary', 'byread'),
-            '-2', mgd.TempInputFile('reads2', 'bylibrary', 'byread'),
-            '--realignments', mgd.TempOutputFile('realignments', 'bylibrary', 'byread'),
-        ),
-    )
-
-    # Calculate likelihoods based on realignments
-
-    workflow.transform(
-        name='calculate_realignment_likelihoods',
-        axes=('bylibrary', 'byread'),
-        ctx=medmem,
-        func=destruct.predict_breaks.calculate_realignment_likelihoods,
-        args=(
-            mgd.TempInputFile('breakpoints_2'),
-            mgd.TempInputFile('realignments', 'bylibrary', 'byread'),
-            mgd.TempInputFile('score.stats', 'bylibrary'),
-            mgd.TempOutputFile('likelihoods_2', 'bylibrary', 'byread'),
-            config['match_score'],
-            mgd.TempInputObj('stats', 'bylibrary').prop('fragment_length_mean'),
-            mgd.TempInputObj('stats', 'bylibrary').prop('fragment_length_stddev'),
-        ),
-    )
-
-    workflow.transform(
-        name='merge_likelihoods_1',
+    workflow.subworkflow(
+        name='realign',
+        func=realign,
         axes=('bylibrary',),
-        ctx=lowmem,
-        func=destruct.tasks.merge_sorted_files_by_line,
         args=(
-            mgd.TempInputFile('likelihoods_2', 'bylibrary', 'byread'),
+            mgd_reads_1.as_input(),
+            mgd_reads_2.as_input(),
+            mgd.TempInputFile('clusters'),
+            mgd.TempInputFile('breakpoints_2'),
             mgd.TempOutputFile('likelihoods_2', 'bylibrary'),
-            mgd.TempSpace('merge_likelihoods_1_temp', 'bylibrary'),
-            '1',
+            mgd.TempInputFile('spanning.alignments', 'bylibrary'),
+            mgd.TempInputFile('score.stats', 'bylibrary'),
+            mgd.TempInputObj('stats', 'bylibrary'),
+            mgd.TempInputObj('library_id', 'bylibrary'),
+            config,
         ),
     )
 
@@ -502,4 +594,3 @@ def create_destruct_workflow(
     )
 
     return workflow
-
