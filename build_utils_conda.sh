cebb600409a1839979ebdefe87785c8a9e8027a4
Andrew McPherson <andrew.mcpherson@gmail.com>
2020-12-17 10:30:24 -0500

    build: utils conda build

--- build_utils_conda.sh
+++ build_utils_conda.sh
@@ -4,8 +4,9 @@ conda config --set always_yes true
 conda config --add channels https://conda.anaconda.org/dranew
 conda config --add channels https://conda.anaconda.org/shahcompbio
 conda config --add channels 'bioconda'
-conda install conda-build anaconda-client
+conda install conda-build
 conda build conda/destruct_utils
+conda install anaconda-client==1.6.14
 anaconda -t $CONDA_UPLOAD_TOKEN upload /usr/local/conda-bld/linux-64/destruct*.tar.bz2
 
 
--- run_build_utils_conda.sh
+++ run_build_utils_conda.sh
@@ -1,1 +1,1 @@
-docker run -v ${PWD}:/repo -w /repo -e CONDA_UPLOAD_TOKEN=$CONDA_UPLOAD_TOKEN -it conda/miniconda2-centos6 bash -e build_utils_conda.sh
+docker run -v ${PWD}:/repo -w /repo -e CONDA_UPLOAD_TOKEN=$CONDA_UPLOAD_TOKEN -it conda/miniconda3-centos7 bash -e build_utils_conda.sh
