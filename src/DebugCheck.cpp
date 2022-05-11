28807eb61af5a2acbaf5317e46493a0bc9d64838
Andrew McPherson <andrew.mcpherson@gmail.com>
2022-05-11 16:55:54 -0400

    bugfix: tabs in reference sequence names should be removed

--- src/DebugCheck.cpp
+++ src/DebugCheck.cpp
@@ -6,10 +6,11 @@
 #include "DebugCheck.h"
 #include "stacktrace.h"
 
+#include <string>
 #include <iostream>
 #include <stdlib.h>
 
-void _ReportFailure(const char* expr, const char* file, int line)
+void _ReportFailure(const std::string& expr, const char* file, int line)
 {
 	std::cerr << "Error: " << expr << " failed on line: " << line << " of " << file << std::endl;
 	print_stacktrace();
