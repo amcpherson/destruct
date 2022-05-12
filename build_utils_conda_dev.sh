0ede9a3fd51ec87bbcd7a0aeb870ec4714020a83
Andrew McPherson <andrew.mcpherson@gmail.com>
2022-05-12 12:16:37 -0400

    build: update conda meta, build scripts, https instead of git protocol

--- build_utils_conda_dev.sh
+++ build_utils_conda_dev.sh
@@ -1,5 +1,5 @@
 
-yum install gcc-c++ git -y
+yum install gcc-c++ libquadmath-devel git -y
 conda config --set always_yes true
 conda config --add channels https://conda.anaconda.org/dranew
 conda config --add channels https://conda.anaconda.org/shahcompbio
@@ -9,7 +9,7 @@ conda install -y scons boost_lib==1.60.0 tclap==1.2.1 bzip2
 cd src
 export CPLUS_INCLUDE_PATH=$PREFIX/include
 export LIBRARY_PATH=$PREFIX/lib
-scons build
+scons
 cd ..
 
 export LD_LIBRARY_PATH=/usr/local/lib/
--- run_build_utils_conda_dev.sh
+++ run_build_utils_conda_dev.sh
@@ -1,1 +1,1 @@
-docker run -v ${PWD}:/repo -w /repo -it conda/miniconda2-centos6 bash -e build_utils_conda_dev.sh
+docker run -v ${PWD}:/repo -w /repo -it conda/miniconda3-centos7 bash -e build_utils_conda_dev.sh
