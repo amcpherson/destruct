6807d2f5fedf4c48f5afc262c1c4fcc270747d7f
Andrew McPherson <andrew.mcpherson@gmail.com>
2022-05-12 21:22:58 -0400

    build: script updates

--- build_utils_conda.sh
+++ build_utils_conda.sh
@@ -4,7 +4,7 @@ conda config --set always_yes true
 conda config --add channels https://conda.anaconda.org/dranew
 conda config --add channels https://conda.anaconda.org/shahcompbio
 conda config --add channels 'bioconda'
-conda install conda-build
+conda install conda-build conda-verify
 conda build conda/destruct_utils
-conda install anaconda-client==1.6.7
+conda install anaconda-client
 anaconda -t $CONDA_UPLOAD_TOKEN upload /usr/local/conda-bld/linux-64/destruct*.tar.bz2
